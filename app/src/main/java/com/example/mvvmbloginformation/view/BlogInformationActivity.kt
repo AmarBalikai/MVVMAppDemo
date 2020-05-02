b319807bc8f44f607ea5771040119a4fbbc45909
Amar Balikai <amar.balikai108@gmail.com>
2020-05-02 13:21:03 +0530

    Added Repository like Glide for Image loading, Retrofit for network call, ViewModel, CardView, size dimens. Changed name of MainActivity to BlogInformation.

--- app/src/main/java/com/example/mvvmbloginformation/view/BlogInformationActivity.kt
+++ app/src/main/java/com/example/mvvmbloginformation/view/BlogInformationActivity.kt
@@ -1,9 +1,10 @@
-package com.example.mvvmbloginformation
+package com.example.mvvmbloginformation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmbloginformation.R
 
-class MainActivity : AppCompatActivity() {
+class BlogInformationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
